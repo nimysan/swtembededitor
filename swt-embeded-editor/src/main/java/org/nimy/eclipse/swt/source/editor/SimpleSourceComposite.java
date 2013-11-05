f2775fed2b8b5c5c35661d87cbe29f188b4d3cc5
234aini <234aini@gmail.com>
2013-11-05 23:00:35 +0800

    fix line number problem

--- swt-embeded-editor/src/main/java/org/nimy/eclipse/swt/source/editor/SimpleSourceComposite.java
+++ swt-embeded-editor/src/main/java/org/nimy/eclipse/swt/source/editor/SimpleSourceComposite.java
@@ -48,7 +48,7 @@ public class SimpleSourceComposite extends Composite {
 		this.styledText.addLineStyleListener(new LineStyleListener() {
 			public void lineGetStyle(LineStyleEvent event) {
 				int lineNumber = SimpleSourceComposite.this.styledText.getLineAtOffset(event.lineOffset);
-				event.bulletIndex = (lineNumber);
+				event.bulletIndex = (lineNumber + 1);
 
 				StyleRange style = new StyleRange();
 				style.foreground = Display.getDefault().getSystemColor(9);
