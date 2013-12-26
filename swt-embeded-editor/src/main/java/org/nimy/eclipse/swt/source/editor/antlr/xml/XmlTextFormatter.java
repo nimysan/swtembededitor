2a23172c81981ca4718e14adffa1ab1d888ccff0
234aini <234aini@gmail.com>
2013-12-26 20:13:11 +0800

    format xml as html to prevent the selfclosing tag

--- swt-embeded-editor/src/main/java/org/nimy/eclipse/swt/source/editor/antlr/xml/XmlTextFormatter.java
+++ swt-embeded-editor/src/main/java/org/nimy/eclipse/swt/source/editor/antlr/xml/XmlTextFormatter.java
@@ -1,15 +1,18 @@
 package org.nimy.eclipse.swt.source.editor.antlr.xml;
 
 import java.io.StringReader;
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.nimy.eclipse.swt.source.editor.formatter.TextFormatter;
 import org.nimy.eclipse.swt.source.editor.utils.Debug;
 import org.w3c.dom.Node;
-import org.w3c.dom.bootstrap.DOMImplementationRegistry;
-import org.w3c.dom.ls.DOMImplementationLS;
-import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.InputSource;
 
 public class XmlTextFormatter implements TextFormatter {
@@ -19,20 +22,22 @@ public class XmlTextFormatter implements TextFormatter {
 		try {
 			InputSource src = new InputSource(new StringReader(input));
 			Node document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(src).getDocumentElement();
-			Boolean keepDeclaration = Boolean.valueOf(input.startsWith("<?xml"));
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.METHOD, "html");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			DOMSource source = new DOMSource(document);
+			StringWriter stringWriter = new StringWriter();
+			StreamResult result = new StreamResult(stringWriter);
+			transformer.transform(source, result);
 
-			DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
-			DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
-			LSSerializer writer = impl.createLSSerializer();
-
-			writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE);
-
-			writer.getDomConfig().setParameter("xml-declaration", keepDeclaration);
-
-			return writer.writeToString(document);
+			return stringWriter.toString();
 		} catch (Exception e) {
+			e.printStackTrace();
 			Debug.error(e);
 		}
 		return input;
 	}
+
 }
